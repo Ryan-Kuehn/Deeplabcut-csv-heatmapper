5b158ff3939fb181d09ffd9557a41dcf43620119
Ryan-Kuehn <168859428+Ryan-Kuehn@users.noreply.github.com>
2024-05-21 16:48:00 -0600

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,19 @@
 import csv
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import *
+
+"""
+file_get: GUI prompt for the path of users CSV
+what_is_graphed: Reads in the users CSV and pulls what to graph
+graph_minsize_x: Determines the minimum X value for our graph
+graph_maxsize_x: Determines the maximum X value for our graph
+graph_minsize_y: Determines the minimum Y value for our graph
+graph_maxsize_y: Determines the maximum Y value for our graph
+x_values: Compiles our x values into a list
+y_values: Compiles our Y values into a list
+
+"""
 
 
 def file_get():
@@ -10,6 +23,62 @@ def file_get():
     file_path = filedialog.askopenfilename()
 
     return file_path
+
+
+def what_is_graphed(file_input):
+    with open(file_input, mode='r') as file:
+
+        # Initial loop to isolate the variables row
+        loop = 0
+        for col in csv.reader(file, delimiter=','):
+            if loop == 1:
+                line = col
+            elif loop == 2:
+                break
+            loop = loop + 1
+
+        # Loop to parse the variables row into options for the GUI
+        options = []
+        val_loop = 0
+        for val in line:
+            parser = val_loop % 3
+            if parser == 0:
+                valid = True
+            else:
+                valid = False
+
+            if val_loop != 0 and valid:
+                options.append(val)
+            val_loop = val_loop + 1
+
+            # Nested function to check when an item in the GUI is selected
+
+            def check_list():
+                try:
+                    selected = lb.selection_get()
+                    print('selected:', selected)
+                    top.destroy()
+                    return selected
+                except:
+                    print('no selection')
+
+        # Loop to display the variables row in the GUI
+        print(options)
+        top = Tk()
+        lb = Listbox(top)
+
+        lb_loop = 1
+        for val in options:
+            lb.insert(lb_loop, val)
+            lb_loop = lb_loop + 1
+        lb.pack()
+
+        # Button to finish and return selection
+        but = Button(top, text="Finish", command=check_list)
+        but.pack()
+        top.mainloop()
+        selected = check_list()
+        print(selected)
 
 
 # x-value functions
@@ -38,7 +107,7 @@ def graph_maxsize_x(file_input):
         for col in csv.reader(file, delimiter=','):
             if float(col[1]) > high:
                 high = float(col[1])
-                high = int(high+50)
+                high = int(high + 50)
     return high
 
 
@@ -68,7 +137,7 @@ def graph_maxsize_y(file_input):
         for col in csv.reader(file, delimiter=','):
             if float(col[2]) > high:
                 high = float(col[2])
-                high = int(high+50)
+                high = int(high + 50)
     return high
 
 
--- main.py
+++ main.py
@@ -2,35 +2,38 @@ import matplotlib.pyplot as plt
 from matplotlib import collections
 import functions as funct
 
+
 file_path = funct.file_get()
-# x-axis setup functions see functions.py
-x_low = funct.graph_minsize_x(file_path)
-x_high = funct.graph_maxsize_x(file_path)
-print(f"x lowest value = {x_low} , x highest value = {x_high}")
+print(funct.what_is_graphed(file_path))
 
-# y-axis setup functions see functions.py
-y_low = funct.graph_minsize_y(file_path)
-y_high = funct.graph_maxsize_y(file_path)
-print(f"y lowest value = {y_low} , y highest value = {y_high}")
-
-plt.style.use('_mpl-gallery-nogrid')
-# data import from csv
-x = funct.x_values(file_path)
-y = funct.y_values(file_path)
-
-# plot:
-fig, ax = plt.subplots()
-
-# args for .hexbin can be found at
-# https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.hexbin.html#matplotlib.axes.Axes.hexbin
-graph = ax.hexbin(x, y, C=None, bins='log', gridsize=50, cmap='jet', edgecolors='grey', marginals=False)
-ax.set(xlim=(x_low, x_high), ylim=(y_high, y_low))
-
-# sets colorbar settings
-cb = fig.colorbar(graph, ax=ax, label='counts')
-
-# displays an array of values for the hexagons
-array = collections.PolyCollection.get_array(graph)
-print(array)
-
-plt.show()
+# # x-axis setup functions see functions.py
+# x_low = funct.graph_minsize_x(file_path)
+# x_high = funct.graph_maxsize_x(file_path)
+# print(f"x lowest value = {x_low} , x highest value = {x_high}")
+#
+# # y-axis setup functions see functions.py
+# y_low = funct.graph_minsize_y(file_path)
+# y_high = funct.graph_maxsize_y(file_path)
+# print(f"y lowest value = {y_low} , y highest value = {y_high}")
+#
+# plt.style.use('_mpl-gallery-nogrid')
+# # data import from csv
+# x = funct.x_values(file_path)
+# y = funct.y_values(file_path)
+#
+# # plot:
+# fig, ax = plt.subplots()
+#
+# # args for .hexbin can be found at
+# # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.hexbin.html#matplotlib.axes.Axes.hexbin
+# graph = ax.hexbin(x, y, C=None, bins='log', gridsize=50, cmap='jet', edgecolors='grey', marginals=False)
+# ax.set(xlim=(x_low, x_high), ylim=(y_high, y_low))
+#
+# # sets colorbar settings
+# cb = fig.colorbar(graph, ax=ax, label='counts')
+#
+# # displays an array of values for the hexagons
+# array = collections.PolyCollection.get_array(graph)
+# print(array)
+#
+# plt.show()
